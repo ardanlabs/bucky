0d6919c6cfc792839d0be67213b22d72fd0a6d02
leogoesger <leoq91@gmail.com>
2026-08-21 13:35:38 -0700

    fix: test

--- version_test.go
+++ version_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestVersion(t *testing.T) {
-	const want = "1.0.8"
+	const want = "1.0.9"
 	if got := Version(); got != want {
 		t.Errorf("Version() = %q, want %q", got, want)
 	}
